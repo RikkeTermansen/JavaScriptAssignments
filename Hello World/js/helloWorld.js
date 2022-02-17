6e2c09429d0c29365dc95dd349ef8f20312f6791
RikkeTermansen <rikke-hermansen1@live.dk>
2022-02-17 09:52:06 +0100

    tilføjet nye opgaver

---
+++ Hello World/js/helloWorld.js
@@ -0,0 +1,14 @@
+console.log("Hello World");
+const switchBtn = document.getElementById("switchBtn");
+let darkmode = true;
+const switchColors = () => {
+document.body.classList.toggle("lightMode");
+if (darkmode) {
+darkmode = false;
+switchBtn.innerText = "Switch to dark mode";
+} else {
+darkmode = true;
+switchBtn.innerText = "Switch to light mode";
+}
+}
+switchBtn.addEventListener("click", switchColors);
