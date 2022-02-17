40ea4d195b918efcad5bbaf25eae12fef632b5eb
RikkeTermansen <rikke-hermansen1@live.dk>
2022-02-17 10:52:12 +0100

    tilføjet en opgave mere

---
+++ 2 numbers/js/main.js
@@ -0,0 +1,10 @@
+let numberOne = 10
+let numberTwo = 5
+
+if (numberOne > numberTwo){
+    console.log (numberOne + "er størst");
+} else if (numberOne < numberTwo){
+    console.log (numberTwo + "er størst");
+} else {
+    console.log ("De er ens");
+};
